f4c924b1dd3dd47c6e085125c292f561309dccbd
Topia99 <1242911982z@gmail.com>
2023-07-25 20:25:13 -0700

    String[] args

--- 1.IntroJava/1.2_Object/Dog.java
+++ 1.IntroJava/1.2_Object/Dog.java
@@ -16,4 +16,18 @@ public class Dog {
         }
 
     }
+
+    public static Dog maxDog(Dog d1, Dog d2){
+        if (d1.weightInPounds > d2.weightInPounds){
+            return d1;
+        }
+        return d2;
+    }
+
+    public Dog maxDog(Dog d2){
+        if (this.weightInPounds > d2.weightInPounds){
+            return this;
+        }
+        return d2;
+    }
 }
--- 1.IntroJava/1.2_Object/DogLauncher.java
+++ 1.IntroJava/1.2_Object/DogLauncher.java
@@ -1,7 +1,12 @@
 public class DogLauncher {
     public static void main(String[] args){
-        Dog d = new Dog(9);
-        d.makeNoise();
+        Dog d1 = new Dog(8);
+        Dog d2 = new Dog(50);
+
+        // Dog bigger = Dog.maxDog(d1, d2);
+        Dog bigger = d1.maxDog(d2);
+        
+        bigger.makeNoise();
         
     }
 }
