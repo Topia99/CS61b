ea433a6f3c1958e162b36f8c66b2b06d2dfd9d07
Topia99 <98057042+Topia99@users.noreply.github.com>
2023-08-14 10:19:10 -0700

    Improve AList

--- 2.Lists/AList.java
+++ 2.Lists/AList.java
@@ -4,26 +4,26 @@
  * getLast: The item we want ot return is in position size -1.
  * size: The number of items in the list should be size.
  */
-public class AList {
-    private int[] items;
+public class AList<T> {
+    private T[] items;
     private int size;
 
     /** Creates an empty list. */
     public AList() {
-        items = new int[100];
+        items = (T[]) new Object[100];
         size = 0;
     }
 
     /** Resizes the underlying array to the target capacity. */
     private void resize() {
-            int[] a = new int[size * 2];
+            T[] a = (T[]) new Object[size * 2];
             System.arraycopy(items, 0, a, 0, size);
             items = a;
-        }
     }
+    
 
     /** Inserts x into the back of the list. */
-    public void addLast(int x){
+    public void addLast(T x){
         // Resize array if size reach to its maximum.
         if (size == items.length) {
             resize();
@@ -33,20 +33,21 @@ public class AList {
     }
 
     /** Returns the item from the back of the List. */
-    public int getLast(){
+    public T getLast(){
         return items[size-1];
     }
 
     /** Returns ith item in the List (0 is the front). */
-    public int get(int i){
+    public T get(int i){
         return items[i];
     }
 
     /** Removes item from the back of the list and 
      * returns deleted item.
      */
-    public int removeLast() {
-        int x = getLast();
+    public T removeLast() {
+        T x = getLast();
+        items[size -1] = null; // Important to do so in generic type to free memory.
         size = size -1;
         return x;
     }
